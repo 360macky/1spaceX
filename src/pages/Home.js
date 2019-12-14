4607b1a8a59f7af3e4004b9ab8137969240d6a49
Marcelo Arias <mail.marcelo.as@gmail.com>
2019-12-13 21:50:23 -0500

    Added Home design: search bar, title and description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 class Home extends React.Component {
   render() {
-    return <h1>...</h1>;
+    return (
+      <div>
+        <div className="text-center m-4">
+          <h1 className="display-4">Cápsulas de SpaceX</h1>
+          <p>
+            Un problema bastante frecuente es que se te haya perdido alguna
+            cápsula de SpaceX.
+            <br />
+            Don't worry. We have a SpaceX Capsules Search for you <span>❤</span>
+          </p>
+        </div>
+        <div className="m-4">
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                name="search"
+                className="form-control w-75 m-auto"
+                autoComplete="off"
+              />
+              <div className="d-flex justify-content-center m-2">
+                <button className="btn btn-dark btn-lg" type="submit">
+                  Buscar cápsula
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Home
+export default Home;
