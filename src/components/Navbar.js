2664691b3a8599bf5eeebbe4d9176674af84a03e
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-03-19 16:02:15 -0500

    Change background color from dark to black

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "black"}}>
         <Link className="navbar-brand h1 m-0" to="/">
           <span role="img" aria-label="rocket">🚀</span>
         </Link>
