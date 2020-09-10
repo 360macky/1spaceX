3dbec9611168cf14225ea01e4e8ca15318d9d16e
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-10 06:44:48 -0500

    Separate Home and Capsules into two different components

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,15 @@ import Navbar from '../components/Navbar';
 import Home from '../pages/Home';
 import Footer from '../components/Footer';
 
+import Capsules from '../pages/capsules/Capsules';
+
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/capsules" component={Capsules} />
       </Switch>
       <Footer />
     </BrowserRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,134 +1,26 @@
 import React from 'react';
 
-import MainThead from './home/MainThead';
-import ResultRow from './home/ResultRow';
-
-function isEmptyObject(object) {
-  for (let key in object) {
-    if (object.hasOwnProperty(key)) {
-      return false;
-    }
-  }
-  return true;
-}
-
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: '',
-      capsulesFounded: {},
-      isLoadingData: null,
-    };
-  }
-
-  handleChange = (event) => {
-    const { value, name } = event.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    this.setState({
-      isLoadingData: true,
-    });
-
-    const SPACEX_API__CAPSULES = 'https://api.spacexdata.com/v3/capsules';
-
-    fetch(SPACEX_API__CAPSULES)
-      .then((response) => response.json())
-      .then((data) => {
-        let details = null;
-        const capsules_founded = data.filter((capsule) => {
-          details = capsule.details;
-          if (details !== null) {
-            let searchRegexp = new RegExp(this.state.search, 'i');
-
-            if (details.search(searchRegexp) !== -1) {
-              return details;
-            }
-          }
-          return false;
-        });
-
-        this.setState({
-          isLoadingData: false,
-          capsulesFounded: capsules_founded,
-        });
-      });
-  };
-
-  render() {
-    let results = null;
-    if (!isEmptyObject(this.state.capsulesFounded)) {
-      results = this.state.capsulesFounded.map((capsule, index) => (
-        <ResultRow
-          key={index}
-          capsule_id={capsule.capsule_id}
-          details={capsule.details}
-          landings={capsule.landings}
-          status={capsule.status}
-          type={capsule.type}
-        />
-      ));
-    }
-    return (
-      <div className="cover vh-100">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <div className="container">
-                <form onSubmit={this.handleSubmit}>
-                  <div className="form-group d-flex flex-column justify-content-center align-items-center">
-                    <input
-                      type="text"
-                      name="search"
-                      value={this.state.search}
-                      onChange={this.handleChange}
-                      className="form-control bg-transparent text-light mt-5 w-50 font-custom"
-                      style={{ fontSize: '1.3rem' }}
-                      autoComplete="off"
-                      spellCheck="false"
-                      placeholder="For example: CRS1"
-                      autoFocus
-                    />
-                    <button
-                      className="btn btn-light btn-lg mt-3 font-custom shadow-lg"
-                      type="submit"
-                      disabled={this.state.search ? false : true}
-                    >
-                      {this.state.isLoadingData === true ? (
-                        <span>Loading...</span>
-                      ) : (
-                        <span>Search capsule</span>
-                      )}
-                    </button>
-                  </div>
-                </form>
-                <table className="table table-responsive table-bordered shadow-sm">
-                  <MainThead results={results} />
-                  {results}
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className="text-center text-light mt-5 w-100 text-center font-custom font-weight-light"
-          style={{ position: 'absolute', marginBottom: '0px' }}
-        >
-          <p>
-            A fairly frequent problem is that you have missed a SpaceX capsule.
-            <br />
-            Don't worry. We have a SpaceX Capsules Search for you <span>❤</span>
-          </p>
-        </div>
+function Home() {
+  return (
+    <div className="cover min-vh-100 d-flex justify-content-center align-items-center">
+      <div>
+        <h1 className="display-4 font-custom text-white text-center">
+          Find SpaceX capsules
+        </h1>
+        <p className="text-white text-center h4">
+          With SpaceX Capsules Search you can find awesome space capsules
+        </p>
+        <p className="lead text-center">
+          <a
+            href="/capsules"
+            className="btn btn-light btn-lg mt-3 font-custom shadow-lg"
+          >
+            Search now!
+          </a>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Home;
