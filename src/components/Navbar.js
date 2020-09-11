3961285b16f788f3aa0d45755dd726c787de335e
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-11 09:03:58 -0500

    Change text align to center with Bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,13 @@ class Navbar extends React.Component {
   render() {
     return (
       <nav
-        className="navbar sticky-top navbar-expand-lg navbar-dark"
-        style={{ backgroundColor: 'black' }}
+        className="navbar sticky-top navbar-expand-lg navbar-dark d-flex justify-content-center align-items-center bg-black"
       >
         <a className="navbar-brand h1 m-0 font-weight-light" href="/">
           <span role="img" className="mr-2" aria-label="rocket">
             <FontAwesomeIcon icon={faRocket} />
           </span>
-          SpaceX Capsules
+          SpaceX Search
         </a>
       </nav>
     );
