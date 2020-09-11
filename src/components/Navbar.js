0ffe251725f48614a73fa0c39e3147261b8f4642
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-11 09:05:47 -0500

    Transform class component to function in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRocket } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav
-        className="navbar sticky-top navbar-expand-lg navbar-dark d-flex justify-content-center align-items-center bg-black"
-      >
-        <a className="navbar-brand h1 m-0 font-weight-light" href="/">
-          <span role="img" className="mr-2" aria-label="rocket">
-            <FontAwesomeIcon icon={faRocket} />
-          </span>
-          SpaceX Search
-        </a>
-      </nav>
-    );
-  }
+function Navbar() {
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark d-flex justify-content-center align-items-center bg-black">
+      <a className="navbar-brand h1 m-0 font-weight-light" href="/">
+        <span role="img" className="mr-2" aria-label="rocket">
+          <FontAwesomeIcon icon={faRocket} />
+        </span>
+        SpaceX Search
+      </a>
+    </nav>
+  );
 }
 
 export default Navbar;
