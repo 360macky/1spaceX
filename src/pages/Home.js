e8496904b5393cc9e2d92c8d21c2f2c5a079db92
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-11 09:35:37 -0500

    Change the title and slogan of the landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,10 @@ function Home() {
     <div className="cover--home min-vh-100 d-flex justify-content-center align-items-center">
       <div>
         <h1 className="display-4 font-custom text-white text-center">
-          Find SpaceX capsules
+          Find SpaceX things.
         </h1>
         <p className="text-white text-center h4">
-          With SpaceX Capsules Search you can find awesome space capsules
+          Like capsules or launches...
         </p>
         <div className="lead text-center">
           <a
