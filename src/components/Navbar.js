f41288c2cf90e570a32ba86d0f57f50e526e4cf6
Marcelo Arias <mail.marcelo.as@gmail.com>
2021-04-16 22:51:38 -0500

    Change <a> tag to <Link> component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRocket } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 function Navbar() {
@@ -8,12 +9,12 @@ function Navbar() {
       className="navbar sticky-top navbar-expand-lg navbar-dark d-flex justify-content-center align-items-center bg-black"
       style={{ backgroundColor: 'black' }}
     >
-      <a className="navbar-brand h1 m-0 font-weight-light" href="/">
+      <Link className="navbar-brand h1 m-0 font-weight-light" to="/">
         <span role="img" className="mr-2" aria-label="rocket">
           <FontAwesomeIcon icon={faRocket} />
         </span>
         SpaceX Search
-      </a>
+      </Link>
     </nav>
   );
 }
