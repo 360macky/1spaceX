dc5eff125150d0700a5b2d6342369d7e7e31608f
Marcelo Arias <hola@marceloarias.com>
2021-11-14 20:52:36 -0500

    Update Cores with the new Search components

--- src/pages/cores/Cores.js
+++ src/pages/cores/Cores.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { isEmptyObject } from '../../utils/isEmptyObject';
+import {
+  ResultsContainer,
+  SearchContainer,
+  SearchInput,
+  SearchButton,
+} from '../../components/Search';
 
 class Cores extends React.Component {
   constructor(props) {
@@ -79,46 +85,14 @@ class Cores extends React.Component {
       ));
     }
     return (
-      <div className="cover min-vh-100">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <div className="container">
-                <form onSubmit={this.handleSubmit}>
-                  <div className="form-group d-flex flex-column justify-content-center align-items-center">
-                    <input
-                      type="text"
-                      name="search"
-                      value={this.state.search}
-                      onChange={this.handleChange}
-                      className="form-control bg-transparent text-light mt-5 w-50 font-custom"
-                      style={{ fontSize: '1.3rem' }}
-                      autoComplete="off"
-                      spellCheck="false"
-                      placeholder="For example: EXPENDED"
-                      autoFocus
-                    />
-                    <button
-                      className="btn btn-light btn-lg mt-3 font-custom shadow-lg"
-                      type="submit"
-                      disabled={this.state.search ? false : true}
-                    >
-                      {this.state.isLoadingData === true ? (
-                        <span>Loading...</span>
-                      ) : (
-                        <span>Search core</span>
-                      )}
-                    </button>
-                  </div>
-                </form>
-                <div className="d-flex flex-wrap justify-content-center align-items-start m-3">
-                  {results}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SearchContainer onSubmit={this.handleSubmit}>
+        <SearchInput value={this.state.search} onChange={this.handleChange} />
+        <SearchButton
+          isLoadingData={this.state.isLoadingData}
+          lookingFor="core"
+        />
+        <ResultsContainer>{results}</ResultsContainer>
+      </SearchContainer>
     );
   }
 }
