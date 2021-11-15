8f27faa926ed9847806149c209f7ea4cfa25c530
Marcelo Arias <hola@marceloarias.com>
2021-11-14 20:42:12 -0500

    Create Search components

---
+++ src/components/Search.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export const SearchContainer = ({ children, onSubmit }) => {
+  return (
+    <div className="cover min-vh-100">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <div className="container">
+              <form onSubmit={onSubmit}>
+                <div className="form-group d-flex flex-column justify-content-center align-items-center">
+                  {children}
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const ResultsContainer = ({ children }) => {
+  return (
+    <div className="d-flex flex-wrap justify-content-center align-items-start m-3">
+      {children}
+    </div>
+  );
+};
+
+export const SearchInput = ({ value, onChange }) => {
+  return (
+    <input
+      type="text"
+      name="search"
+      value={value}
+      onChange={onChange}
+      className="form-control bg-transparent text-light mt-5 w-50 font-custom"
+      style={{ fontSize: '1.3rem' }}
+      autoComplete="off"
+      spellCheck="false"
+      placeholder="For example: EXPENDED"
+      autoFocus
+    />
+  );
+};
+
+export const SearchButton = ({ children }) => {
+  return (
+    <button
+      className="btn btn-light btn-lg mt-3 font-custom shadow-lg"
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
