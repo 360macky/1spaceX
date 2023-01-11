66a9def3fc9acaa79dd903e77fff80051d3295cf
Marcelo Arias <hello@marceloarias.com>
2023-01-10 19:44:08 -0500

    Add service workers and new icons

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './global.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App />);
+
+serviceWorkerRegistration.register();
