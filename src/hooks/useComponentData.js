0dc39eb300afa0e84629400e413edb530ea5df27
Marcelo Arias <hello@marceloarias.com>
2023-01-23 15:33:29 -0500

    Create useComponentData custom hook

---
+++ src/hooks/useComponentData.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+/**
+ * @name useComponentData
+ * @description Hook to get data from API or localStorage
+ * @param {string} item - Item to get from localStorage
+ * @param {string} api - API to get data
+ * @returns {Object} Data from API or localStorage in JSON format
+ */
+export default function useComponentData({ item, api }) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const f = async () => {
+      const dataInLocalStorage = localStorage.getItem(item);
+      if (dataInLocalStorage) {
+        setData(JSON.parse(dataInLocalStorage));
+      } else {
+        const response = await fetch(api);
+        const data = await response.json();
+        setData(data);
+      }
+    };
+    f();
+    return () => {};
+  }, []);
+  return data;
+}
