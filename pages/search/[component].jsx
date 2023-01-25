b476e78ad896b64b5ac7b8c672454ecd0ae31964
Marcelo Arias <hello@marceloarias.com>
2023-01-25 17:11:56 -0500

    Fix search engine in Capsules/Cores components

--- pages/search/[component].jsx
+++ pages/search/[component].jsx
@@ -52,22 +52,20 @@ const Capsules = () => {
     switch (component) {
       case 'capsules':
         return result.filter((capsule) => {
-          if (capsule.capsule_id !== null && capsule.capsule_serial !== null) {
+          if (capsule.serial !== null && capsule.last_update !== null) {
             return (
-              capsule.capsule_id.toLowerCase().includes(search.toLowerCase()) ||
-              capsule.capsule_serial
-                .toLowerCase()
-                .includes(search.toLowerCase())
+              capsule.serial.toLowerCase().includes(search.toLowerCase()) ||
+              capsule.last_update.toLowerCase().includes(search.toLowerCase())
             );
           }
           return false;
         });
       case 'cores':
         return result.filter((core) => {
-          if (core.core_serial !== null && core.details !== null) {
+          if (core.serial !== null && core.last_update !== null) {
             return (
-              core.core_serial.toLowerCase().includes(search.toLowerCase()) ||
-              core.details.toLowerCase().includes(search.toLowerCase())
+              core.serial.toLowerCase().includes(search.toLowerCase()) ||
+              core.last_update.toLowerCase().includes(search.toLowerCase())
             );
           }
           return false;
